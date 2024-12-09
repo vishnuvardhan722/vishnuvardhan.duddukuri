1fdd464869a57a6bf27856e0688bcc176f36757a
vishnuvardhan722 <65936107+vishnuvardhan722@users.noreply.github.com>
2024-12-09 18:08:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ function toggleMenu() {
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  document.getElementById("vishnu").checkVisibility(false);
+},
+
+#vishnu{
+  Display: none;
 }
